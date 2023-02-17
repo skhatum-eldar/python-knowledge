51778b7b65d49bde4962faa3a40143f1a7738b7a
Eldar Skhatum <79996321123@ya.ru>
2023-02-18 00:23:38 +0300

    6th task

--- Homework1.py
+++ Homework1.py
@@ -16,9 +16,29 @@
 # s = int(input('Enter the total number of cranes: '))
 # print(f'Petya and Seryoja did {s // 6} cranes, Katya did {s * 2 // 3} cranes')
 
+
 # Задача 6: Вы пользуетесь общественным транспортом? Вероятно, вы расплачивались за проезд и получали билет с номером. Счастливым билетом называют такой билет с шестизначным номером, где сумма первых трех цифр равна сумме последних трех. Т.е. билет с номером 385916 – счастливый, т.к. 3+8+5=9+1+6. Вам требуется написать программу, которая проверяет счастливость билета.
 # 385916 -> yes
 # 123456 -> no
+# ticket_number = int(input("Enter a 6-digit ticket number: "))
+# first_3 = ticket_number // 1000
+# sum1 = 0
+# while first_3 > 0:
+#     digit = first_3 % 10
+#     sum1 += digit
+#     first_3 //= 10
+
+# last_3 = ticket_number % 1000
+# sum2 = 0
+# while last_3 > 0:
+#     digit = last_3 % 10
+#     sum2 += digit
+#     last_3 //= 10
+
+# if sum1 == sum2:
+#     print('WOOHOO! YOUR TICKET IS VERY LUCKY!')
+# else:
+#     print('DONT BE SAD! EVEN IF THE TICKET IS UNLUCKY!')
 
 # Задача 8: Требуется определить, можно ли от шоколадки размером n × m долек отломить k долек, если разрешается сделать один разлом по прямой между дольками (то есть разломить шоколадку на два прямоугольника).
 # 3 2 4 -> yes
