c20ece3f3d12ebd2f07c0c827ee3fefac37a9948
Eldar Skhatum <79996321123@ya.ru>
2023-02-18 00:33:32 +0300

    8th task

--- Homework1.py
+++ Homework1.py
@@ -43,3 +43,12 @@
 # Задача 8: Требуется определить, можно ли от шоколадки размером n × m долек отломить k долек, если разрешается сделать один разлом по прямой между дольками (то есть разломить шоколадку на два прямоугольника).
 # 3 2 4 -> yes
 # 3 2 1 -> no
+n = int(input("Enter the number of rows of the chocolate bar: "))
+m = int(input("Enter the number of columns of the chocolate bar: "))
+k = int(input("Enter the number of pieces you want to break off: "))
+
+total = n * m
+if k <= total and (total - k) % 2 == 0:
+    print(f'YES! ITS POSSIBLE TO BREAK OFF {k} PIECES!')
+else:
+    print(f'NOPE! ITS NOT POSSIBLE TO BREAK OFF {k} PIECES!')
