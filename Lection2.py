3ed273931f5712b3e04ccb0721d7f619ae28ff67
Eldar Skhatum <79996321123@ya.ru>
2023-02-19 00:43:56 +0300

    commit

---
+++ Lection2.py
@@ -0,0 +1,96 @@
+# list_1 = []
+# list_1 = list()
+# list_1 = [1, 2, 3, 8]
+# print(*list_1)
+# print(list_1)
+
+# for i in list_1:
+#     print(i)
+
+# print(len(list_1))
+# print(list_1[0])
+# print(list_1[-2])
+
+# list_1 = [1, 5]
+# print(list_1)
+# list_1.append(8)
+# print(list_1)
+# list_1.append(85)
+# print(list_1)
+
+# list_1 = []
+# print(list_1)
+# for i in range (5):
+#     list_1.append(i)
+#     print(list_1)
+
+# Удаление последнего элемента списка.
+# list_1 = [12, 7, -1, 21, 0]
+# print(list_1.pop()) # 0
+# print(list_1) # [12, 7, -1, 21]
+# print(list_1.pop()) # 21
+# print(list_1) # [12, 7, -11
+# print(list_1.pop()) # - 1
+# print(list_1) # [12, 7]
+
+# Удаление конкретного элемента из списка
+# list_1 = [12, 7, -1, 21, 0]
+# print(list_1.pop(0)) # 12
+# print(list_1) # [7, -1, 21, 0]
+
+# Добавление элемента на нужную позицию
+# list_1 = [12, 7, -1, 21, 0]
+# print(list_1.insert(2, 11))
+# print(list_1) # [12, 7, 11, -1, 21, 0]
+
+# list_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print(list_1[0])    #1
+# print(list_1[1])    #2
+# print(list_1[-1])   #10
+# print(list_1[-5])   #6
+# print(list_1[:])    # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print(list_1[:2])   # [1, 2]
+# print(list_1[len(list_1)-2:])   #[9, 10]
+# print(list_1[2:9])  # [3, 4, 5, 6, 7, 8, 9]
+# print(list_1[6:-18])    # []
+# print(list_1[0:len(list_1):6])  #[1,7]
+# print(list_1[::6])  #[1,7]
+
+# t = ()
+
+# print(type(t))
+
+# t = (1, 5, 3,)
+# print (type(t))
+
+# v = [1, 8, 9]
+# print(v)
+# print(type(v))
+
+# v = tuple(v)
+# print(v)
+# print(type(v))
+
+# a,b,c = v
+# print(a, b, c)
+
+# t = (1, 2, 3, 5,)
+
+# for i in range(len(t)):
+#     print(t[i])
+
+# t = [1, 2, 3, 5]
+
+# t[0] = 2
+
+# print(t)
+
+d = {}
+
+d = dict()
+
+d['q'] = 'qwerty'
+print(d)
+
+d['w'] = 'werty'
+print(d['w'])
